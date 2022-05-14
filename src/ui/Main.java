ce8dd217fc482f3765a3999c72b5617bcb1e90af
Ryan <ryanmtan5@gmail.com>
2022-05-14 01:21:45 -0700

    Create main

---
+++ src/ui/Main.java
@@ -0,0 +1,16 @@
+package ui;
+
+import model.Inventory;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    static void displayMenu() {
+
+    }
+
+}
